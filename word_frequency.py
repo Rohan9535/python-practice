7db917cdec173997a84b9baf11977989eeb1d272
ROHAN <168158712+Rohan9535@users.noreply.github.com>
2026-08-09 11:35:56 +0530

    Add files via upload

---
+++ word_frequency.py
@@ -0,0 +1,11 @@
+text = input("Enter a sentence: ")
+
+words = text.lower().split()
+frequency = {}
+
+for word in words:
+    frequency[word] = frequency.get(word, 0) + 1
+
+print("Word Frequency:")
+for word, count in frequency.items():
+    print(word, ":", count)
